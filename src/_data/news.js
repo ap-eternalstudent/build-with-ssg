9c2f857c1df2070c9dec60ee0715926eb9869d01
anthony28 <anthony@wearea2.com>
2021-02-26 16:37:48 -0500

    descriptive env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = async function () {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`
+      `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
     return response.data;
   } catch (error) {
